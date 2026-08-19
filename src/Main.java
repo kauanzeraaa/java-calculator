aaaeda472875db4999a1f0fae3a21d2e4666d07e
kauanzeraaa <piacentekauan@gmail.com>
2026-08-18 22:18:43 -0300

    Replace user input with hardcoded values for testing in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Digite o primeiro número: ");
-        double numero1 = sc.nextDouble();
-
-        System.out.print("Digite o segundo número: ");
-        double numero2 = sc.nextDouble();
+        double numero1 = 25.6;
+        double numero2 = 12.8;
 
         Main calculator = new Main();
 
